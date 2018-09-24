5ee273957bd6d9ffd68974a6f674ffa01267e1fd
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-24 18:56:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function BoardMember(name, homeState, training){
   BoardMember.prototype.approve = function(){
     return 'You can do that!'
   }
-  this.doCharity = function(){
+
+  BoardMember.prototype.doCharity = function(){
     return "I like to help people."
   }
 
